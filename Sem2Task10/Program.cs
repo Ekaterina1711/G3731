3dc534731f2b374ea542e696ce5b3286652dc185
Ekaterina <a9084235@yandex.ru>
2023-01-19 18:31:02 +0300

    add solution for task 9,10,11,12,14,16

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// System.Random numSintezator = new System.Random();
+// char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+// Console.WriteLine(digits);
+// int digit1 = (int)digits[0]-48;
+// int digit2 = (int)digits[1]-48;
+// int resNum = digit1>digit2?digit1:digit2;
+// Console.WriteLine(resNum);
